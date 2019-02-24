771e08634d79e054681e548431d34e0bab7674f8
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-02-24 19:01:34 +0100

    First App In Spring

---
+++ src/main/java/com/example/FirstAppInSpring/FirstAppInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstAppInSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppInSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/FirstAppInSpring/controlers/MainControler.java
@@ -0,0 +1,18 @@
+package com.example.FirstAppInSpring.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainControler {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index (Model model){
+        model.addAttribute("name", "Jan");
+        model.addAttribute("surname","Kowalski");
+        return "index";
+    }
+}
